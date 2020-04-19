073edfa6cc0d8790f23dc1c64428a06f1219ea94
aaronblac <32806041+aaronblac@users.noreply.github.com>
2020-04-18 14:46:02 -1000

    update keyup for mobile

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var userClickedPattern = [];
 var level = 0;
 var started = false;
 
-$(document).keypress(function(){
+$(document).keyup(function(){
   if (!started){
 $("#level-title").text("Level " + level);
 nextSequence();
