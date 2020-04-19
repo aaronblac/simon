3d4c1269f5c012ab964ba60af2200f29fb1c0969
aaronblac <32806041+aaronblac@users.noreply.github.com>
2020-04-18 15:00:55 -1000

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ var userClickedPattern = [];
 var level = 0;
 var started = false;
 
-$(document).keydown(function(){
+$(document).click(function(){
   if (!started){
 $("#level-title").text("Level " + level);
 nextSequence();
